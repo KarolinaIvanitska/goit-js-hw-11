727189e3ec2617891d66e991d5acaf98b60d719c
Karolina Zaiats <karolina.ivanitska@gmail.com>
2024-03-11 12:40:41 +0100

    k

--- src/js/render-function.js
+++ src/js/render-function.js
@@ -1,4 +1,23 @@
-export function renderPhoto({
+function displayImages(images) {
+  galleryList.innerHTML = '';
+
+  if (images.length === 0) {
+    iziToast.info({
+      title: 'Info',
+      message:
+        'Sorry, there are no images matching your search query. Please try again.',
+    });
+    return;
+  }
+
+  const markup = createMarkup(images);
+  galleryList.innerHTML = markup;
+
+  lightbox.refresh();
+  hideLoader();
+}
+
+export function imagesTemplate({
   webformatURL,
   largeImageUrl,
   tags,
@@ -7,6 +26,33 @@ export function renderPhoto({
   comments,
   downloads,
 }) {
-  return `
-    <li class`;
+  return ` 
+  <li class="gallery-item">
+  <a class="gallery-link" href="${largeImageURL}">
+    <img
+      class="gallery-image"
+      src="${webformatURL}"
+      alt="${tags}"
+      width="360"
+    />
+  </a>
+  <div class="thumb-block">
+    <div class="block">
+      <h2 class="title">Likes</h2>
+      <p class="amount">${likes}</p>
+    </div>
+    <div class="block">
+      <h2 class="title">Views</h2>
+      <p class="amount">${views}</p>
+    </div>
+    <div class="block">
+      <h2 class="title">Comments</h2>
+      <p class="amount">${comments}</p>
+    </div>
+    <div class="block">
+      <h2 class="title">Downloads</h2>
+      <p class="amount">${downloads}</p>
+    </div>
+  </div>
+</li>`;
 }
